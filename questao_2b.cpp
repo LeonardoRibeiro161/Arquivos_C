ce043b7cb39529babe60f9cac6579923bdc287b2
Leonardo Ribeiro <48728011+LeonardoRibeiro161@users.noreply.github.com>
2019-09-28 13:29:52 -0300

    Correção do codigo

--- questao_2b.cpp
+++ questao_2b.cpp
@@ -10,9 +10,8 @@ int main()
 	FILE *p_file;
 	char conteudo[100];
 	char aux[100];
-	char letras[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	char diretorio[80] = "C:\\Users\\leonardo.ribeiro-nub\\Desktop\\teste.txt";
-	int i,w,z;
+	char diretorio[80] = "C:\\Users\\Leonardo\\Desktop\\AlunosFap2019.txt";
+	int i;
 	p_file = fopen(diretorio, "r");
 	
 	if(p_file == NULL)
@@ -25,40 +24,23 @@ int main()
 		cout<<"Conteudo criptogrado"<<endl;
 		fread(&conteudo, sizeof(char), sizeof(conteudo), p_file);
 		cout<<conteudo<<endl;
-		
-		for(int i  = 0; i < strlen(conteudo); i++)
+		cout<<"Conteudo descriptografado"<<endl;
+		for(i = 0; i < strlen(conteudo);i++)
 		{
-			w = 0; 
-			while(conteudo[i] != letras[w])
-			{
-				w++;
-			}
-			conteudo[i] = w;
-		}
-
-		for(i = 1; i <= 26; i++)
-		{
-			for(z = 0; z < strlen(conteudo); z++ )
-			{
-				w = conteudo[z] + i;
-				if(w >= 26)
-				{
-					w -= 26;
-				}
-				if(letras[i] == 'b','o','l','a')
-				{
-					cout<<letras[w];
-				}		
-			}
-			cout<<endl;
 			
+			if(conteudo[i] == 'a')
+			{
+				aux[i] = 'z';
+			}
+			else
+			{
+				aux[i] = conteudo[i] -1;
+			}
 		}
-	
 		
-		
+		cout<<aux;
 		
 		fclose(p_file);
 	}
 }
-
 
